f091bf27448e79594c69c2846de58a528bbec5bd
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-09-09 10:59:50 +0530

    layout(add): feedback layout, turn off auth

---
+++ layouts/feedback.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col h-screen overflow-hidden">
+    <NavTop />
+    <div class="flex-grow flex relative w-full h-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
+3
--- pages/feedback/index.vue
+++ pages/feedback/index.vue
@@ -2,14 +2,25 @@
   <div class="py-32">
     <button class="p-2 rounded-md bg-emerald-500 text-white" @click="getFeedback">Get Feedback</button>
     <h1>Add the kanban here</h1>
+    <p>{{ feedbacks }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-const getFeedback = () => {
-  // get feedback from storage
+const feedbacks = ref({})
+
+const getFeedback = async () => {
+  const { data, error } = await useFetch('/api/feedback')
+  if (error.value) {
+    console.error(error.value)
+  }
+  console.log(data)
+  feedbacks.value = data
 }
 
+definePageMeta({
+  layout: 'feedback',
+})
 </script>
 
 <style scoped></style>
