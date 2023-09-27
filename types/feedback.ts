bae0691b900d1556ddcf6fc7fb1068b49f96152c
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-09-27 09:22:35 +0530

    fix(zod): extend error

--- types/feedback.ts
+++ types/feedback.ts
@@ -7,7 +7,7 @@ export const feedbackSubmittedSchema = z.object({
   category_id: z.number(),
 })
 
-export const feedbackSchema = z.extend(feedbackSubmittedSchema).object({
+export const feedbackSchema = feedbackSubmittedSchema.extend({
   created_at: z.string(),
   updated_at: z.string(),
   votes_weighted: z.number(),
